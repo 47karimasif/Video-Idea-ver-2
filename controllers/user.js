87f3558caa20c8f1f243eb2f8087450e03068c8f
47karimasif <karimasif98@gmail.com>
2019-12-11 02:46:30 +0530

    secured

--- controllers/user.js
+++ controllers/user.js
@@ -107,9 +107,10 @@ const sendTokenResponse = (user,res) =>{
     const options = {
         expires:new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE *24*60*60*1000),
         httpOnly:true,
-        secure = true
     };
-    
+    if(process.env.NODE_ENV==="production") {
+        options.secure = true;
+    }
     res.cookie("token",token,options)
 }
 
